47a6110c4bddf3e0c0146226baea610d85a5442e
Mateusz Zdanowicz <mat309@o2.pl>
2016-01-05 16:25:18 +0100

    request access token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
   def index
-    @id = ENV['github_id']
-    @secret = ENV['github_secret']
+    @id = get_id
+    @secret = get_secret
+  end
+
+  def oauth
+    uri = URI('https://github.com/login/oauth/access_token')
+    res = Net::HTTP.post_form(uri, "client_id":get_id,"client_secret":get_secret,"code":params[:code])
+    session[:access_token] = res.body
+    render plain: res.body
+  end
+
+  private
+  def get_id
+    ENV['github_id']
+  end
+
+  def get_secret
+    ENV['github_secret']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   
   get 'pages/index'
+
+  get '/oauth', to: 'pages#oauth'
 end
