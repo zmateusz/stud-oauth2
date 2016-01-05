76b6c212496750002d585f86911ab5f5c548f2f7
Mateusz Zdanowicz <mat309@o2.pl>
2016-01-05 16:59:20 +0100

    add logout method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,17 @@ class PagesController < ApplicationController
     uri = URI('https://github.com/login/oauth/access_token')
     res = Net::HTTP.post_form(uri, "client_id":get_id,"client_secret":get_secret,"code":params[:code])
     session[:access_token] = res.body
+    redirect_to pages_user_path
+  end
+
+  def logout
+    session[:access_token] = nil
+    redirect_to root_path
+  end
+
+  def user
+    uri = URI("https://api.github.com/user")
+    res = Net::HTTP.post_form(uri, "access_token":session[:access_token])
     render plain: res.body
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#index'
   
   get 'pages/index'
+  get 'pages/logout'
+  get 'pages/user'
 
   get '/oauth', to: 'pages#oauth'
 end
